8f7a23904dfb1ecff7c9fcf47f0e9604439d113a
Davide Marchiori <mrcdvd77@gmail.com>
2024-09-13 10:04:59 +0200

    edit

--- move-dist.js
+++ move-dist.js
@@ -19,6 +19,27 @@ const path = require("path");
 const sourceDir = path.join(__dirname, "out");
 const destDir = path.join(__dirname, "dist");
 
+function copyFolderRecursiveSync(source, target) {
+  // Check if folder needs to be created or integrated
+  const targetFolder = path.join(target, path.basename(source));
+  if (!fs.existsSync(targetFolder)) {
+    fs.mkdirSync(targetFolder, { recursive: true });
+  }
+
+  // Copy
+  if (fs.lstatSync(source).isDirectory()) {
+    const files = fs.readdirSync(source);
+    files.forEach(function (file) {
+      const curSource = path.join(source, file);
+      if (fs.lstatSync(curSource).isDirectory()) {
+        copyFolderRecursiveSync(curSource, targetFolder);
+      } else {
+        fs.copyFileSync(curSource, path.join(targetFolder, file));
+      }
+    });
+  }
+}
+
 function deleteFolderRecursive(dir) {
   if (fs.existsSync(dir)) {
     fs.readdirSync(dir).forEach((file) => {
@@ -40,8 +61,14 @@ try {
     deleteFolderRecursive(destDir);
   }
 
-  // Rename 'out' directory to 'dist'
-  fs.renameSync(sourceDir, destDir);
+  // Copy 'out' directory to 'dist'
+  console.log('Copying "out" directory to "dist"');
+  copyFolderRecursiveSync(sourceDir, path.dirname(destDir));
+
+  // Remove 'out' directory
+  console.log('Removing "out" directory');
+  deleteFolderRecursive(sourceDir);
+
   console.log('Successfully moved "out" directory to "dist"');
 } catch (error) {
   console.error("Error moving directory:", error);
