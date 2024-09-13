bf2319287a608a5fb905419a832a4cb207411872
Davide Marchiori <mrcdvd77@gmail.com>
2024-09-13 11:28:30 +0200

    only mac and msi

--- move-dist.js
+++ move-dist.js
@@ -1,18 +1,3 @@
-// const fs = require("fs");
-// const path = require("path");
-
-// const sourceDir = path.join(__dirname, "out");
-// const destDir = path.join(__dirname, "dist");
-
-// try {
-//   // Rename 'out' directory to 'dist'
-//   fs.renameSync(sourceDir, destDir);
-//   console.log('Successfully moved "out" directory to "dist"');
-// } catch (error) {
-//   console.error("Error moving directory:", error);
-//   process.exit(1);
-// }
-
 const fs = require("fs");
 const path = require("path");
 
